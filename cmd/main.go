e84e81f1c486ac6c115d5e13877b8ea7a3375f22
Cian Gallagher <cian.gallagher@zendesk.com>
2021-12-12 23:06:05 +0000

    MAIN: Abstract functionality & boilerplate to watch cmd.

--- cmd/main.go
+++ cmd/main.go
@@ -1,23 +1,5 @@
 package main
 
-import (
-	"github.com/cian911/switchboard/watcher"
-)
-
 func main() {
-	path := "/Users/cian.gallagher/input"
-	destination := "/Users/cian.gallagher/output"
-
-	var pw watcher.Producer = &watcher.PathWatcher{
-		Path: path,
-	}
-
-	var pc watcher.Consumer = &watcher.PathConsumer{
-		Path:        path,
-		Destination: destination,
-		Ext:         ".txt",
-	}
-
-	pw.Register(&pc)
-	pw.Observe()
+	Watch()
 }

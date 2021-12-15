8184697117fc8f8431915d07839da5e41bb4ee21
Cian Gallagher <cian.gallagher@zendesk.com>
2021-12-15 22:48:06 +0000

    SPECS: Add specs for events.

--- event/event_test.go
+++ event/event_test.go
@@ -1,1 +1,35 @@
 package event
+
+import "testing"
+
+var (
+	event = &Event{
+		File:        "readme.txt",
+		Path:        "/input",
+		Destination: "/output",
+		Ext:         ".txt",
+		Operation:   "CREATE",
+	}
+
+	ext = ".txt"
+)
+
+func TestEvent(t *testing.T) {
+	t.Run("It returns true when event is valid", func(t *testing.T) {
+		want := true
+		got := event.IsValidEvent(ext)
+
+		if want != got {
+			t.Errorf("event extension is not valid, when it should have been: want=%t, got=%t", want, got)
+		}
+	})
+
+	t.Run("It returns false when event is valid", func(t *testing.T) {
+		want := false
+		got := event.IsValidEvent(".mp4")
+
+		if want != got {
+			t.Errorf("event extension is not valid, when it should have been: want=%t, got=%t", want, got)
+		}
+	})
+}

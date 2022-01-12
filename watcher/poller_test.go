215e551ef6e6cec7d9a7ce395751e4dc47273cf6
Cian Gallagher <cian.gallagher@zendesk.com>
2022-01-12 23:27:55 +0000

    WATCHER: Add more poller tests. Add additional logging in watcher.

--- watcher/poller_test.go
+++ watcher/poller_test.go
@@ -3,6 +3,8 @@ package watcher
 import (
 	"testing"
 	"time"
+
+	"github.com/cian911/switchboard/event"
 )
 
 var (
@@ -26,10 +28,39 @@ func TestPoller(t *testing.T) {
 			t.Errorf("Queue size did not decrease. want=%d, got=%d", 0, pw.Queue.Size())
 		}
 	})
+
+	t.Run("It successfully notifies on a new dir event", func(t *testing.T) {
+		pw := setupPathwatcher("/tmp")
+		pw.Poll(pollInterval)
+
+		ev := setupNewDirEvent(t)
+		pw.Queue.Add(*ev)
+
+		if pw.Queue.Size() != 1 {
+			t.Errorf("Queue size did not increase. want=%d, got=%d", 1, pw.Queue.Size())
+		}
+		<-time.After(3 * time.Second)
+
+		if pw.Queue.Size() != 0 {
+			t.Errorf("Queue size did not decrease. want=%d, got=%d", 0, pw.Queue.Size())
+		}
+	})
 }
 
 func setupPathwatcher(path string) *PathWatcher {
 	return &PathWatcher{
 		Queue: NewQueue(),
+	}
+}
+
+func setupNewDirEvent(t *testing.T) *event.Event {
+	path := t.TempDir()
+
+	return &event.Event{
+		Path:        path,
+		Destination: t.TempDir(),
+		Ext:         ".txt",
+		Operation:   "CREATE",
+		Timestamp:   time.Now(),
 	}
 }

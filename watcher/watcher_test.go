5f4641232aeaf11523b2ba41b1860f4fd999669a
Cian Gallagher <cian@ciangallagher.net>
2022-03-11 19:07:45 +0000

    TEST: Add test for regex events

--- watcher/watcher_test.go
+++ watcher/watcher_test.go
@@ -31,7 +31,7 @@ var (
 
 func TestWatcher(t *testing.T) {
 	t.Run("It registers a consumer", func(t *testing.T) {
-		pw, pc := setup(path, destination, ext)
+		pw, pc := setup(path, destination, ext, "")
 
 		pw.Register(&pc)
 
@@ -41,7 +41,7 @@ func TestWatcher(t *testing.T) {
 	})
 
 	t.Run("It unregisters a consumer", func(t *testing.T) {
-		pw, pc := setup(path, destination, ext)
+		pw, pc := setup(path, destination, ext, "")
 
 		pw.Register(&pc)
 		pw.Unregister(&pc)
@@ -57,7 +57,7 @@ func TestWatcher(t *testing.T) {
 		ev.Path = t.TempDir()
 		ev.File = utils.ExtractFileExt(ev.Path)
 
-		pw, pc := setup(ev.Path, ev.Destination, ev.Ext)
+		pw, pc := setup(ev.Path, ev.Destination, ev.Ext, "")
 		pw.Register(&pc)
 		pw.Unregister(&pc)
 
@@ -82,17 +82,52 @@ func TestWatcher(t *testing.T) {
 			t.Fatalf("want: %d != got: %d - debug - files: %v, event: %v", want, got, filesInDir, ev)
 		}
 	})
+
+	t.Run("It pocesses a pattern matched event", func(t *testing.T) {
+		ev := eventSetup(t)
+		ev.File = utils.ExtractFileExt(ev.Path)
+
+		pw, pc := setup(ev.Path, ev.Destination, ev.Ext, "[0-9]+.txt")
+		pw.Register(&pc)
+		pw.Unregister(&pc)
+
+		for i := 1; i <= 3; i++ {
+			// Create 3 temp files
+			file := createTempFile(ev.Path, ".txt", t)
+			defer os.Remove(file.Name())
+		}
+
+		pc.Receive(ev.Path, "CREATE")
+
+		// Scan dest dir for how many files it contains
+		// if want == got, all files have been moved successfully
+		filesInDir, err := utils.ScanFilesInDir(ev.Destination)
+		t.Logf("Files in Dir: %v", filesInDir)
+		if err != nil {
+			t.Fatalf("Could not scan all files in destination dir: %v", err)
+		}
+
+		want := 3
+		got := len(filesInDir)
+
+		if want != got {
+			t.Fatalf("want: %d != got: %d - debug - files: %v, event: %v", want, got, filesInDir, ev)
+		}
+	})
 }
 
-func setup(p, d, e string) (Producer, Consumer) {
+func setup(p, d, e, rp string) (Producer, Consumer) {
 	var pw Producer = &PathWatcher{
 		Path: p,
 	}
+
+	regexPattern, _ := utils.ValidateRegexPattern(rp)
 
 	var pc Consumer = &PathConsumer{
 		Path:        p,
 		Destination: d,
 		Ext:         e,
+		Pattern:     *regexPattern,
 	}
 
 	return pw, pc
